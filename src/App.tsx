0bc7f4be41293613c83aed498216bccbe8a82933
Yuri Corrêa <yuri.correa1@outlook.com>
2023-09-07 02:50:05 -0300

    feat: create initial structure project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    aalignItems: 'center',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
